c96db9ad0450284d2bdada435aba8d048399613e
Chandan Veerabhadrappa <chandanv89@gmail.com>
2023-12-06 00:13:04 +0000

    Updated GlobalExceptionHandler and ImageUrlNotFoundException

--- api/src/main/java/com/github/chandanv89/api/periodictable/exception/ImageUrlNotFoundException.java
+++ api/src/main/java/com/github/chandanv89/api/periodictable/exception/ImageUrlNotFoundException.java
@@ -1,11 +1,8 @@
 package com.github.chandanv89.api.periodictable.exception;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ImageUrlNotFoundException extends RuntimeException {
   private final String elementName;
   private final String message;
